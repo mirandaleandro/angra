63ce80499d2a007a94d9b9fad7347a1a29fed64e
amurray4 <acmurray425@gmail.com>
2014-03-12 21:18:37 -0600

    # Please enter the commit message for your changes. Lines starting # with '#' will be ignored, and an empty message aborts the commit. # On branch master # Changes to be committed: #   (use "git reset HEAD <file>..." to unstage) # #	modified:   app/models/Migrations.scala #	modified:   app/models/User.scala #

---
+++ app/models/Request.scala
@@ -0,0 +1,26 @@
+package models
+import PostgresConnection._
+
+
+
+class Request(var user_id:User, var ret_date:java.util.Date, var ret_location:String, var ret_time:java.util.Date, var comments:String) extends Entity
+
+object Request
+{
+  def apply(user_id:User,  ret_date:java.util.Date, ret_location:String, ret_time:java.util.Date, comments:String) =
+  transactional
+  {
+    new Request(user_id,ret_date,ret_location,ret_time,comments)
+  }
+
+  def findById(id: String) = byId[Request](id)
+
+  def findByUser(user_id:User): Option[Request] = transactional
+  {
+    (select[Request] where(_.user_id :== user_id)).headOption
+  }
+
+
+  def getAll:List[Request] = all[Request]
+
+}
