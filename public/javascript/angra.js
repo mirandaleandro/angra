8b525b1d6fe138d617529f164af3687dfc2183a7
Leandro Miranda <mirandaleandro@gmail.com>
2014-03-15 18:40:18 -0600

    Additional transport

--- public/javascript/angra.js
+++ public/javascript/angra.js
@@ -1,27 +1,48 @@
 $(function(){
 
-    $('.accordion-collapse').on('show.bs.collapse', function (n) {
+    var accordion = $('.accordion-collapse');
+    var tagInputs =  $('input[data-role=tagsinput], select[multiple][data-role=tagsinput]');
+
+    accordion.on('show.bs.collapse', function (n) {
         $(n.target).siblings('.accordion-toggle').toggleClass('open');
     });
 
-    $('.accordion-collapse').on('hide.bs.collapse', function (n) {
+    accordion.on('hide.bs.collapse', function (n) {
         $(n.target).siblings('.accordion-toggle').toggleClass('open');
     });
 
-//    $('#airlines').tagsinput({
-//        typeahead: {
-//            source: ['Amsterdam', 'Washington', 'Sydney', 'Beijing', 'Cairo'],
-//            freeInput: true
-//        }
-//    });
+    tagInputs.tagsinput();
 
-    $('#airlines').tagsinput();
-    // Adding custom typeahead support using http://twitter.github.io/typeahead.js
-    $('#airlines').tagsinput('input').typeahead({
-        prefetch: '/assets/json/companies.json'
-    }).bind('typeahead:selected', $.proxy(function (obj, datum) {
+    tagInputs.each(function()
+    {
+        var tagInput = $(this);
 
-            this.tagsinput('add', datum.value);
-            this.tagsinput('input').typeahead('setQuery', '');
-        }, $('#airlines')));
+        var contentSource =  tagInput.data('content');
+
+        if(contentSource)
+        {
+            tagInput.tagsinput('input').typeahead(
+                {
+                    prefetch: contentSource
+                })
+                .bind('typeahead:selected', $.proxy(function (obj, datum)
+                {
+                    this.tagsinput('add', datum.value);
+                    this.tagsinput('input').typeahead('setQuery', '');
+                }, tagInput ) )
+        }
+
+    });
+
+    $(document.body).on("change",".additional-transportation", function()
+    {
+        var radio = $(this);
+        var rentalCompanyGroup = radio.closest('.trip').find('.form-group.preferred-rental-company');
+
+        if( radio.hasClass('rental') )
+            rentalCompanyGroup.show('slow');
+        else
+            rentalCompanyGroup.hide('slow')
+    });
+
 });
