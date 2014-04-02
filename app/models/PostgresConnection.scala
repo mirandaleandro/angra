67660c249b012f432a17e9cde83b963d4c5a3af7
Maria Yacaman <yacamanmaria@gmail.com>
2014-04-02 16:55:22 -0600

    Configuration file modified and working.

--- app/models/PostgresConnection.scala
+++ app/models/PostgresConnection.scala
@@ -7,7 +7,7 @@ import net.fwbrasil.activate.storage.relational.idiom.postgresqlDialect
 object PostgresConnection extends ActivateContext {
   override val storage = new PooledJdbcRelationalStorage {
     val jdbcDriver = "org.postgresql.Driver"
-    val user = "d2fsoa6nldt0du"
+    val user = "ibibjyxukawtqo"
     val password = "y9YH2xFG2hfTzJKbytiRspSXAr"
     val url = "jdbc:postgresql://ec2-184-73-194-196.compute-1.amazonaws.com/d2fsoa6nldt0du"
     val dialect = postgresqlDialect
